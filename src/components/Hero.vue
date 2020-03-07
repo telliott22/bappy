4dabf3a8056f9f8de10b873b5cc7415bd652372e
Tim Elliott <telliott22@gmail.com>
2020-03-07 14:37:33 +0000

    :construction: flickering text and signature intro added

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,6 +2,13 @@
   <div class="hero">
 
     <div
+      class="hero__signature"
+      ref="signature"
+    >
+
+    </div>
+
+    <div
       class="hero__left"
       ref="left"
     >
@@ -29,7 +36,7 @@
 </template>
 
 <script>
-import { TimelineLite, Power4 } from "gsap/all";
+import { TimelineMax, Power4, RoughEase } from "gsap/all";
 import Joystick from "./Joystick";
 
 export default {
@@ -39,26 +46,85 @@ export default {
   },
   mounted() {
     const left = this.$refs.left;
-    // const leftImage = this.$refs.leftImage;
+    const leftImage = this.$refs.leftImage;
     const right = this.$refs.right;
+    const rightImage = this.$refs.rightImage;
     const joystick = this.$refs.joystick.$el;
+    const signature = this.$refs.signature;
 
-    let timeline = new TimelineLite({ paused: true });
+    let timeline = new TimelineMax({ paused: true });
 
     let yDuration = "1";
 
-    const viewPortSize = {
-      height: document.documentElement.clientHeight,
-      width: document.documentElement.clientWidth
-    };
+    const viewPortSizeHeight = document.documentElement.clientHeight;
 
+    //Used to calcuate where on the screen the sections will meet vertically
+    let verticalSeperationPoint = 180;
+
+    //Where the left section will animate down to
+    let leftTopFinishPosition = viewPortSizeHeight - verticalSeperationPoint;
+
+    //Get postion of left section
     let leftBoundingClientRect = left.getBoundingClientRect();
 
+    //Move joystick to correct position
+    joystick.style.left = leftBoundingClientRect.right - 58 + "px";
+    joystick.style.top = leftTopFinishPosition - 58 + "px";
+
+    const flickeringTextPoints = 50;
+    const flickeringTextDuration = 0.5;
+
     timeline
+      .to(signature, {
+        opacity: 0,
+        duration: 0,
+        onComplete: () => {
+          signature.style.display = "none";
+        }
+      })
+      .fromTo(
+        leftImage,
+        {
+          opacity: 0
+        },
+        {
+          opacity: 1,
+          duration: flickeringTextDuration,
+          ease: RoughEase.ease.config({
+            template: Power4.easeInOut,
+            strength: 2,
+            points: flickeringTextPoints,
+            taper: "out",
+            randomize: false,
+            clamp: true
+          }),
+          delay: "0.1"
+        },
+        "flickeringText"
+      )
+      .fromTo(
+        rightImage,
+        {
+          opacity: 0
+        },
+        {
+          opacity: 1,
+          duration: flickeringTextDuration,
+          ease: RoughEase.ease.config({
+            template: Power4.easeInOut,
+            strength: 2,
+            points: flickeringTextPoints,
+            taper: "out",
+            randomize: false,
+            clamp: true
+          })
+        },
+        "flickeringText"
+      )
       .to(
         left,
         {
-          top: viewPortSize.height - 200 + "px",
+          top: leftTopFinishPosition + "px",
           duration: yDuration,
           ease: Power4.easeInOut
         },
@@ -67,32 +133,22 @@ export default {
       .to(
         right,
         {
-          bottom: "200px",
+          bottom: verticalSeperationPoint + "px",
           duration: yDuration,
-          ease: Power4.easeInOut,
-          onComplete: function() {
-            leftBoundingClientRect = left.getBoundingClientRect();
-
-            console.log(leftBoundingClientRect);
-
-            console.log(joystick.style);
-
-            joystick.style.left = leftBoundingClientRect.right - 58 + "px";
-            joystick.style.top = leftBoundingClientRect.top - 58 + "px";
-          }
+          ease: Power4.easeInOut
         },
         "animateY"
       )
       .to(joystick, {
         opacity: 1,
         duration: 0.4,
-        ease: "linear",
-        delay: -0.5
+        ease: Power4.easeOut,
+        delay: -0.4
       });
 
     setTimeout(() => {
       timeline.resume();
-    }, 1000);
+    }, 2000);
   }
 };
 </script>
